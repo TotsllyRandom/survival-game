c16a9b50193e02455adf5b61df78d0337bafd847
Totally_RandomYT <68128408+TotsllyRandom@users.noreply.github.com>
2026-06-07 15:47:27 -0500

    Basic Map moving

--- Scripts/tiles.gd
+++ Scripts/tiles.gd
@@ -1,34 +1,38 @@
 extends Node2D
 
-var mouse_placement: Vector2i
-var mouse_offset: Vector2i
-var mouse_down: bool
+var mouse_placement: Vector2
+var mouse_offset: Vector2
+var mouse_down: bool = false
+var real_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	position.x = mouse_offset.x % int(32 * scale.x)
-	position.y = mouse_offset.y % int(32 * scale.y)
+	real_position += mouse_offset
+	for child in get_children():
+		if child.name == "Water":
+			child.position = Vector2(
+				-64 + fposmod(real_position.x, 32.0),
+				-64 + fposmod(real_position.y, 36.0)
+			)
+		else:
+			child.position = Vector2(
+				-64 + real_position.x,
+				-70 + real_position.y
+			)
 	
 	@warning_ignore("int_as_enum_without_cast")
-	if Input.is_mouse_button_pressed(1) and !mouse_down:
+	if Input.is_mouse_button_pressed(1):
+		if mouse_down == false:
+			mouse_placement = get_global_mouse_position()
 		mouse_down = true
-		mouse_placement = get_global_mouse_position()
-		mouse_placement.x -= mouse_offset.x
-		mouse_placement.y -= mouse_offset.y
-	
+	else:
+		mouse_down = false
+		mouse_offset = Vector2(0.0,0.0)
+		
 	if mouse_down:
-		@warning_ignore("int_as_enum_without_cast")
-		if !Input.is_mouse_button_pressed(1):
-			mouse_down = false
-		else:
-			@warning_ignore("narrowing_conversion")
-			mouse_offset.y = get_global_mouse_position().y - mouse_placement.y
-			@warning_ignore("narrowing_conversion")
-			mouse_offset.x = get_global_mouse_position().x - mouse_placement.x
-		
-		
+		mouse_offset = (get_global_mouse_position() - mouse_placement) / scale
+		mouse_placement = get_global_mouse_position()
