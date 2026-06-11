67afc70d020491cb18287fb4112c72aedb2e4a6c
Totally_RandomYT <68128408+TotsllyRandom@users.noreply.github.com>
2026-06-11 00:35:49 -0500

    finished most art, added basic generation

---
+++ Scripts/GlobalTweaks.gd
@@ -0,0 +1,5 @@
+extends Node
+
+## Keep settings here that players can change that tweak world gen. This may go into a world gen seed?
+
+var smoothness = 5
--- Scripts/tiles.gd
+++ Scripts/tiles.gd
@@ -5,9 +5,19 @@ var mouse_offset: Vector2
 var mouse_down: bool = false
 var real_position: Vector2
 
+var def_item = {
+	"tile": 0,
+	"height": 0,
+	"sprite": 0,
+	"rand_val": 1,
+}
+var map_data: Array
+var map_size = Vector2(40,60)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	create_map()
+	print_map()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -21,7 +31,7 @@ func _process(_delta: float) -> void:
 		else:
 			child.position = Vector2(
 				-64 + real_position.x,
-				-70 + real_position.y
+				-64 + real_position.y
 			)
 	
 	@warning_ignore("int_as_enum_without_cast")
@@ -36,3 +46,92 @@ func _process(_delta: float) -> void:
 	if mouse_down:
 		mouse_offset = (get_global_mouse_position() - mouse_placement) / scale
 		mouse_placement = get_global_mouse_position()
+
+func generate_noise() -> Array:
+	var ret = []
+	var current_line = []
+	for y in range(map_size.y):
+		current_line = []
+		for x in range(map_size.x):
+			current_line.append(randi_range(0,2))
+		ret.append(current_line)
+	return ret
+
+func on_edge(x,y) -> bool:
+	var r = true
+	
+	if x < 0:
+		r=false
+	if y<0:
+		r=false
+	if x>=map_size.x - 1:
+		r=false
+	if y>=map_size.y - 1:
+		r=false
+	
+	return r
+
+func get_return(noise,x,y) -> int:
+	var a = 0
+	var b = 0
+	var c = 0
+	
+	for ay in range(y-1,y+2):
+		for ax in range(x-1,x+1):
+			if on_edge(ax,ay):
+				match noise[ay][ax+1]:
+					0:
+						a+=1
+					1:
+						b+=1
+					2:
+						c+=1
+	if on_edge(x+1,y):
+				match noise[y][x+1]:
+					0:
+						a+=1
+					1:
+						b+=1
+					2:
+						c+=1
+
+	if a>b and a>c:
+		return 0
+	if b>a and b>c:
+		return 1
+	if c>a and c>b:
+		return 2
+	return 1
+
+func smooth(noise: Array) -> Array:
+	for y in range(noise.size()):
+		for x in range(noise[0].size()):
+			noise[y][x] = get_return(noise,x,y)
+	return noise
+
+func create_map():
+	var noise = generate_noise()
+	var current_line = []
+	
+	for i in range(GlobalTweaks.smoothness):
+		noise = smooth(noise)
+	
+	map_data = []
+	var current_item = {}
+	for y in range(map_size.y):
+		current_line = []
+		for x in range(map_size.x):
+			current_item = def_item.duplicate()
+			current_item["height"] = noise[y][x]
+			current_item["tile"] = randi_range(1,5)
+			current_item["sprite"] = current_item["tile"] + (current_item["height"] * 6)
+			current_item["rand_val"] = randi_range(0,1)
+			
+			current_line.append(current_item)
+		map_data.append(current_line)
+		
+func print_map():
+	$Real.clear()
+	for y in range(map_size.y):
+		for x in range(map_size.x):
+			$Real.set_cell(Vector2i(x,y),9,Vector2i(map_data[y][x].get("sprite"),0))
