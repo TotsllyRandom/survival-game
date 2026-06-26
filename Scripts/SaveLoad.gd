b8a068f8b1aa7057a12064f73f7617d8c20773f4
Totally_RandomYT <battebery8808@gmail.com>
2026-06-25 23:10:57 -0500

    Starting IOS DEMO Export v 0.1.0
    0.1.0

--- Scripts/SaveLoad.gd
+++ Scripts/SaveLoad.gd
@@ -1,1 +1,23 @@
 extends Node
+
+func load_data():
+	if !FileAccess.file_exists("user://save.json"):
+		return
+	
+	var file = FileAccess.open("user://save.json", FileAccess.READ)
+	var text = file.get_as_text()
+	file.close()
+
+	var json = JSON.new()
+
+	if json.parse(text) == OK:
+		var save = json.data
+		pass
+
+func save_data():
+	var ret = {}
+	ret["resources"] = Player.resources
+	
+	var file = FileAccess.open("user://save.json", FileAccess.WRITE)
+	file.store_string(JSON.stringify(ret))
+	file.close()
